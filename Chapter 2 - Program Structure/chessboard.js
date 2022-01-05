11c19cb6b2200910a1a95d836ee2145992a0f8ee
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-01-06 01:23:39 +0530

    Update chessboard.js

--- Chapter 2 - Program Structure/chessboard.js
+++ Chapter 2 - Program Structure/chessboard.js
@@ -1,5 +1,5 @@
 /*
-3. Chessboard (30/12/2019)
+3. Chessboard
 Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
 Passing this string to console.log should show something like this:
  # # # #
