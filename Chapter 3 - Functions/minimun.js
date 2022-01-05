645da38eee494f01763f9b4d6149c978a1b87fa3
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-01-06 01:27:47 +0530

    Create minimun.js

---
+++ Chapter 3 - Functions/minimun.js
@@ -0,0 +1,4 @@
+/*
+1. Minimum (30/12/2019)
+The 2nd chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+*/
